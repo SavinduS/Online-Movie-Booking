9bec2e31dac940f159de04ab74b84f47455d1e3d
supuna18 <supuna793@gmail.com>
2025-06-22 12:42:16 +0530

    Update UserProfile.php link path

--- UserProfile.php
+++ UserProfile.php
@@ -4,7 +4,7 @@ include 'database/db.php';
 
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
-    header('Location: signin.php');
+    header('Location: UserProfile.php');
     exit();
 }
 
